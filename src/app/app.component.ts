167d7be04016a918cdd2dd61347754f146befdb7
kozake <sh.kozake@gmail.com>
2018-07-29 11:12:02 +0900

    setting firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,16 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyDubXW4Ylga9xJrBr2raEBs45bbPm9xyT0',
+  authDomain: 'awesome-chat-2b88d.firebaseapp.com',
+  databaseURL: 'https://awesome-chat-2b88d.firebaseio.com',
+  projectId: 'awesome-chat-2b88d',
+  storageBucket: 'awesome-chat-2b88d.appspot.com',
+  messagingSenderId: '544074400018'
+};
 
 @Component({
   selector: 'app-root',
@@ -22,5 +32,6 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+    firebase.initializeApp(config);
   }
 }
