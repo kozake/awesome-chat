92736f2621bf8de7e3552698be51ff81e0014a6f
kozake <sh.kozake@gmail.com>
2018-07-29 15:03:50 +0900

    Implemented chat page

--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavController, Content } from '@ionic/angular';
+import * as firebase from 'Firebase';
 
 @Component({
   selector: 'app-chat',
@@ -7,9 +10,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatPage implements OnInit {
 
-  constructor() { }
+  roomkey: string;
+  nickname: string;
+  chatMessage: string;
+
+  chats = [];
+  offStatus = false;
+
+  @ViewChild(Content) content: Content;
+
+  constructor(public navCtrl: NavController, public route: ActivatedRoute) {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.roomkey = this.route.snapshot.paramMap.get('key') as string;
+        this.nickname = user.email;
+
+        this.sendJoinMessage();
+        this.displayChatMessage();
+
+      } else {
+        this.navCtrl.goRoot('signin');
+      }
+    });
+  }
 
   ngOnInit() {
   }
 
+  displayChatMessage() {
+    firebase.database()
+      .ref('chatrooms/' + this.roomkey + '/chats')
+      .on('value', resp => {
+
+        if (resp) {
+          this.chats = [];
+          resp.forEach(childSnapshot => {
+            const chat = childSnapshot.val();
+            chat.key = childSnapshot.key;
+            this.chats.push(chat);
+          });
+          setTimeout(async () => {
+            if (this.offStatus === false) {
+              // FIX-ME
+              // V4でコンテンツエリアをスクロールする方法が分からない
+              const el = await this.content.getScrollElement();
+              el.scrollToBottom(300);
+            }
+          });
+        }
+      });
+  }
+
+  exitChat() {
+    this.sendExitMessage();
+    this.offStatus = true;
+    this.navCtrl.goBack('room');
+  }
+
+  sendChatMessage() {
+    this.sendMessage('message', this.chatMessage);
+  }
+
+  sendJoinMessage() {
+    this.sendMessage('join', this.nickname + ' has joined this room.');
+  }
+
+  sendExitMessage() {
+    this.sendMessage('exit', this.nickname + ' has exited this room.');
+  }
+
+  sendMessage(type: string, message: string) {
+    const newData = firebase.database().ref('chatrooms/' + this.roomkey + '/chats').push();
+    newData.set({
+      type: type,
+      user: this.nickname,
+      message: message,
+      sendDate: Date()
+    });
+  }
 }
